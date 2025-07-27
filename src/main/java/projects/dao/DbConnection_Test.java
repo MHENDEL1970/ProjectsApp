aa058b0a00e3630c5b63dec7e008c6dcd3c86302
MHENDEL1970 <104833675+MHENDEL1970@users.noreply.github.com>
2025-07-27 17:45:56 -0400

    First Commit

---
+++ src/main/java/projects/dao/DbConnection_Test.java
@@ -0,0 +1,7 @@
+package projects.dao;
+
+public class DbConnection_Test {
+    public static void main(String[] args) {
+        DbConnection.getConnection();
+    }
+}
---
+++ src/main/java/projects/dao/TestDbConnection.java
@@ -0,0 +1,12 @@
+package projects.dao;
+
+public class TestDbConnection {
+    public static void main(String[] args) {
+        try {
+            DbConnection.getConnection();
+            System.out.println("Database connection test successful!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
